63b541d4cd8ed27ea2266c1a4dddf0960346ae19
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-06 01:45:26 +0530

    [Harshal]Add:Check For Two String Are Anagram Or Not

---
+++ AlgorithmPrograms/Anagram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class Anagram
+    {
+        public void AnagramOrNot()
+        {
+            Console.WriteLine("Enter 1st string value: ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter 2nd string value: ");
+            string str2 = Console.ReadLine();
+            //get lengths of strings
+            char[] length1 = str1.ToLower().ToCharArray();
+            char[] length2 = str2.ToLower().ToCharArray();
+            if (length1.Length != length2.Length)//length are not same then string is not anagram
+            {
+                Console.WriteLine("Strings Are Not Anagram");
+            }
+            Array.Sort(length1);//sort both string
+            Array.Sort(length2);
+            for (int i = 0; i < length1.Length; i++) //compare sorted string
+            {
+                if (length1[i] != length2[i])
+                {
+                    Console.WriteLine("Enterd String Is Not Anagram\n");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Enterd String Is Anagram\n");
+                    return;
+                }
+            }
+
+        }
+    }
+}
