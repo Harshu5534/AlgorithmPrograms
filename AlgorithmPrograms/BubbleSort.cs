2d077ef99ff6b337853d6bf6576ccbcf5c44d950
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-06 01:36:17 +0530

    [Harshal]Add:Bubble Sorting

---
+++ AlgorithmPrograms/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BubbleSort
+    {
+        public void Bubble()
+        {
+            int[] arr=new int[7] {3,8,7,5,4,9,9};
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j=0;j<arr.Length;j++)
+                { 
+                    if(arr[i] <= arr[j])
+                    {
+                        temp=arr[i]; 
+                        arr[i]=arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After Sorting Array");
+            for (int i=0;i<arr.Length;i++)
+            {
+               Console.Write("{0},\n",arr[i]);
+            }
+        }
+    }
+}
