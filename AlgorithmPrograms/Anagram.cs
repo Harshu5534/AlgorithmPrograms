4b96d3bce59d29d981f1e1d689b2b83c5755bf92
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-06 17:18:16 +0530

    [Harshal]Add:Check For Two String Are Anagram Or Not

--- AlgorithmPrograms/Anagram.cs
+++ AlgorithmPrograms/Anagram.cs
@@ -8,35 +8,44 @@ namespace AlgorithmPrograms
 {
     public class Anagram
     {
+        int count = 0;
         public void AnagramOrNot()
         {
             Console.WriteLine("Enter 1st string value: ");
             string str1 = Console.ReadLine();
             Console.WriteLine("Enter 2nd string value: ");
             string str2 = Console.ReadLine();
-            //get lengths of strings
-            char[] length1 = str1.ToLower().ToCharArray();
-            char[] length2 = str2.ToLower().ToCharArray();
-            if (length1.Length != length2.Length)//length are not same then string is not anagram
+            int first_Length=str1.Length;
+            int second_Length=str2.Length;
+            bool check = true;
+            if(first_Length!=second_Length)
             {
-                Console.WriteLine("Strings Are Not Anagram");
+                check = false;
             }
-            Array.Sort(length1);//sort both string
-            Array.Sort(length2);
-            for (int i = 0; i < length1.Length; i++) //compare sorted string
+            else
             {
-                if (length1[i] != length2[i])
-                {
-                    Console.WriteLine("Enterd String Is Not Anagram\n");
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Enterd String Is Anagram\n");
-                    return;
-                }
-            }
+                char[] first_array=str1.ToLower().ToCharArray();
+                char[] second_array = str2.ToLower().ToCharArray();
+                Array.Sort(first_array);
+                Array.Sort(second_array);
+                check=second_array.Equals(first_array);
 
+                for(int i=0; i<first_Length && first_Length==second_Length; i++)
+                {
+                    if(first_array[i]!=second_array[i])
+                    {
+                        count++;
+                    }
+                }
+            }
+            if(count==0)
+            {
+                Console.WriteLine("Entered Value is anagram");
+            }
+            else
+            {
+                Console.WriteLine("Enterd Value is not anagram");
+            }
         }
     }
 }
